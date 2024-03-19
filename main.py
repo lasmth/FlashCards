2e90660fec601af6e688da1fa3e61a12094f5cf2
Liam <liam.alan.smth@gmail.com>
2024-03-19 21:36:33 +0000

    Loaded french-english dictionary from csv and added random word choice on button press.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import tkinter
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+def choose_new_word():
+    new_word = random.choice(word_translation_dict)
+    new_word_french = new_word["French"]
+    card_canvas.itemconfig(french_word, text=new_word_french)
 
 # --------- UI SETUP ------------ #
 window = tkinter.Tk()
@@ -16,10 +23,15 @@ card_canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 french_word = card_canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 
 right_button_image = tkinter.PhotoImage(file="images/right.png")
-right_button = tkinter.Button(image=right_button_image, bd=0, highlightthickness=0)
+right_button = tkinter.Button(image=right_button_image, bd=0, highlightthickness=0, command=choose_new_word)
 right_button.grid(row=1, column=0)
 wrong_button_image = tkinter.PhotoImage(file="images/wrong.png")
-wrong_button = tkinter.Button(image=wrong_button_image, bd=0, highlightthickness=0)
+wrong_button = tkinter.Button(image=wrong_button_image, bd=0, highlightthickness=0, command=choose_new_word)
 wrong_button.grid(row=1, column=1)
+
+# --------- Reading word data ---------- #
+data = pandas.read_csv("data/french_words.csv")
+# Orient just determines how the dictionary is interpreted from the DataFrame.
+word_translation_dict = data.to_dict(orient="records")
 
 window.mainloop()
