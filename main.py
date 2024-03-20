59d2cbb4efc0ee20bb4006ce7c64c39d6c420dcc
Liam <liam.alan.smth@gmail.com>
2024-03-20 19:31:01 +0000

    Now saves progress.

--- main.py
+++ main.py
@@ -29,6 +29,16 @@ def display_english():
     card_canvas.itemconfig(card_canvas_image, image=card_back_image)
 
 
+def right_button_press():
+    # Should be called when the user presses the 'right' button - i.e. they know the word and don't need to learn it.
+    word_translation_dict.remove(current_word)
+    choose_new_word()
+
+
+def save_data():
+    data_to_save = pandas.DataFrame.from_records(word_translation_dict)
+    data_to_save.to_csv(path_or_buf="data/words_to_learn.csv", index=False)
+
 
 # --------- UI SETUP ------------ #
 window = tkinter.Tk()
@@ -45,14 +55,18 @@ language_text = card_canvas.create_text(400, 150, text="Title", font=("Ariel", 4
 word_text = card_canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 
 right_button_image = tkinter.PhotoImage(file="images/right.png")
-right_button = tkinter.Button(image=right_button_image, bd=0, highlightthickness=0, command=choose_new_word)
+right_button = tkinter.Button(image=right_button_image, bd=0, highlightthickness=0, command=right_button_press)
 right_button.grid(row=1, column=0)
 wrong_button_image = tkinter.PhotoImage(file="images/wrong.png")
-wrong_button = tkinter.Button(image=wrong_button_image, bd=0, highlightthickness=0, command=display_english)
+wrong_button = tkinter.Button(image=wrong_button_image, bd=0, highlightthickness=0, command=choose_new_word)
 wrong_button.grid(row=1, column=1)
 
 # --------- Reading word data ---------- #
-data = pandas.read_csv("data/french_words.csv")
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    print("ok")
+    data = pandas.read_csv("data/french_words.csv")
 # Orient just determines how the dictionary is interpreted from the DataFrame.
 word_translation_dict = data.to_dict(orient="records")
 
@@ -61,3 +75,4 @@ flip_timer = window.after(3000, display_english)
 choose_new_word()
 
 window.mainloop()
+save_data()
